aca7d1e4d964c150bed7beaa14d35d85436d3c0e
avanti77 <avantiak7804@gmail.com>
2024-04-19 23:54:26 +0530

    commit

---
+++ portfolio/src/Details.js
@@ -0,0 +1,201 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/photo2.jpeg";
+// Tech stack images
+import cpp from "./assets/techstack/cpp.png";
+import python from "./assets/techstack/python.jpg";
+import sql from "./assets/techstack/sql.png";
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import nodejs from './assets/techstack/nodejs.jpg';
+import mongodb from './assets/techstack/mongodb.jpg';
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import tensorflow from "./assets/techstack/tensorflow.png";
+import numpy from "./assets/techstack/numpy.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Avantika Khanna",
+  tagline: "I am a Technophile.",
+  img: profile,
+  about: `I am a 3rd year undergraduate student. I have strong problem solving, analytics,decision making and excellent communication with interpersonal skills. I pay attention to details and am able to interpret data and trends,diagnose problems, and formulate effective actions plans to resolve issues. I am self-driven and motivated with entrepreneurial passion and desire to work in a fast-paced, result driven environment with varied responsibilities.
+
+  • I have scored a combined aggregate score of 8 or above across all semesters
+  • I possess an extremely sound understanding in the basic areas of computer science such as Data structures , Algorithms, Object Oriented Programming and Design.
+  • I have a good command on C/C++.
+  
+  Though I don't have much real world experience but I am a keen and quick learner and I am ready to put my 100% in whatever I do.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/avantika-khanna-586783228/",
+  github: "https://github.com/avanti77",
+  twitter: "https://twitter.com/_a_one_7804",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `TBI GEU`,
+    Location: "Dehradun",
+    Type: "Internship",
+    Duration: "April 2024 - June 2024",
+  },
+  // {
+  //   Position: "Internship",
+  //   Company: `Company Name here`,
+  //   Location: "Bengaluru",
+  //   Type: "Internship",
+  //   Duration: "Sep 2021 - Dec 2021",
+  // },
+  // {
+  //   Position: "Internship",
+  //   Company: `Company Name here`,
+  //   Location: "Bengaluru",
+  //   Type: "Internship",
+  //   Duration: "Sep 2021 - Dec 2021",
+  // },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Course: "Bachelor in Computer Science and Engineering",
+    College: `Graphic Era Deemed To Be University`,
+    Location: "Dehradun",
+    Type: "Full Time",
+    Duration: "2021 - Present",
+  },
+  {
+    Course: "Intermediate",
+    College: "St. Mary Senior Secondary School",
+    Location: "Moradabad",
+    Type: "96.4%",
+    Duration: "2021",
+  },
+  {
+    Course: "High School",
+    College: "St. Mary Senior Secondary School",
+    Location: "Moradabad",
+    Type: "97%",
+    Duration: "2019",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  cpp: cpp,
+  python: python,
+  sql: sql,
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  nodejs: nodejs,
+  mongodb: mongodb,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  tensorflow: tensorflow,
+  numpy: numpy,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "FrontEnd of Ecommerce Website",
+    image: projectImage1,
+    description: `Developed FrontEnd of a food delivery application using the react with a focus on user-friendly React components. Implemented features for user authentication, shopping cart functionality`,
+    techstack: "React.js, Redux, CSS",
+    previewLink: "https://github.com/avanti77/Frontend-Food-Ordering-Website",
+    githubLink: "https://github.com/avanti77/Frontend-Food-Ordering-Website",
+  },
+  {
+    title: "Student Result Management System",
+    image: projectImage2,
+    description: `Developed a streamlined Student Result Management System (SRMS) utilizing Python and a database to
+    automate and enhance the management of student academic records and results. Facilitated efficient storage,
+    retrieval, and manipulation of essential student information, fostering improved administrative processes.
+    `,
+    techstack: "Tkinter, Pillow",
+    previewLink: "https://github.com/avanti77/Student-Result-Management-System",
+    githubLink: "https://github.com/avanti77/Student-Result-Management-System",
+  },
+  {
+    title: "Twitter Sentimental Analysis",
+    image: projectImage3,
+    description: `Developed a hate speech detection system for tweets, distinguishing tweets with racist or sexist sentiment
+    from others. Task involves precise classification of tweets into categories of negative sentiment or otherwise.
+    `,
+    techstack: "NLTK , WordCloud , Matplotlib , Seaborn , Pandas , Numpy",
+    previewLink: "https://github.com/avanti77/twitter-sentimental-analysis",
+    githubLink: "https://github.com/avanti77/twitter-sentimental-analysis",
+  },
+  {
+    title: "Next Word Predictor",
+    image: projectImage4,
+    description: `Designed a next-word predictor enhancing user experience in applications like text editors, messaging, search
+    engines, and virtual assistants, by providing accurate and contextually relevant word suggestions for efficient
+    typing.`,
+    techstack: "TensorFlow, Tokenizer, LSTM, pickle, Numpy",
+    previewLink: "https://github.com/avanti77/Next-Word-Predictor",
+    githubLink: "https://github.com/avanti77/Next-Word-Predictor",
+  },
+//   {
+//     title: "Project title 5",
+//     image: projectImage5,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 6",
+//     image: projectImage6,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "avantiak7804@gmail.com",
+  phone: "+91 7505428450",
+};
---
+++ portfolio/src/Pages/Resume.js
@@ -0,0 +1,135 @@
+// import React from 'react';
+// import Resumes from '../assets/avantika___resume.pdf';
+// const Resume = () => {
+//   return (
+//     <div>
+//       <h2>My Resume</h2>
+//       <embed src={Resumes} type="application/pdf" width="100%" height="800px" />
+//     </div>
+//   );
+// }
+
+// export default Resume;
+
+// import React, { useState, useEffect } from "react";
+// import { Container, Row } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
+// // import Particle from "../Particle";
+// import pdf from '../assets/avantika___resume.pdf';
+// // import pdf from "../../Assets/../Assets/Aman_Gupta_Photo_resume.pdf";
+// import { AiOutlineDownload } from "react-icons/ai";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+// function Resume  () {
+//   const [width, setWidth] = useState(1200);
+
+//   useEffect(() => {
+//     setWidth(window.innerWidth);
+//   }, []);
+
+//   return (
+//     <div>
+//       <Container fluid className="resume-section">
+//         {/* <Particle /> */}
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button
+//             variant="primary"
+//             href={pdf}
+//             target="_blank"
+//             style={{ maxWidth: "250px" }}
+//           >
+//             <AiOutlineDownload />
+//             &nbsp;Download Resume
+//           </Button>
+//         </Row>
+
+//         <Row className="resume">
+//           <Document file={pdf} className="d-flex justify-content-center">
+//             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+//           </Document>
+//         </Row>
+
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button
+//             variant="primary"
+//             href={pdf}
+//             target="_blank"
+//             style={{ maxWidth: "250px" }}
+//           >
+//             <AiOutlineDownload />
+//             &nbsp;Download Resume
+//           </Button>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default Resume;
+
+
+// import React from 'react';
+// import { FaFilePdf } from 'react-icons/fa';
+// import Resumes from '../assets/avantika___resume.pdf';
+
+// const Resume = () => {
+//   return (
+//     <div className="container mx-auto px-4 py-8">
+//       <div className="max-w-lg mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+//         <div className="flex items-center justify-center bg-gray-800">
+//           <FaFilePdf className="text-white text-6xl" />
+//         </div>
+//         <div className="p-6">
+//           <h2 className="text-2xl font-semibold text-gray-800">My Resume</h2>
+//           <div className="mt-4">
+//             <a
+//               href={Resumes}
+//               download="avantika_resume.pdf"
+//               className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg mr-4"
+//             >
+//               Download PDF
+//             </a>
+//             <div className="mt-4">
+//               <embed src={Resumes} type="application/pdf" width="100%" height="800px" />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Resume;
+
+
+import React from 'react';
+import Resumes from '../assets/avantika___resume.pdf';
+import { FaFilePdf } from 'react-icons/fa';
+
+const Resume = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-lg mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+        <div className="p-6">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">My Resume</h2>
+          <div className="resume-container">
+            <embed src={Resumes} type="application/pdf" width="100%" height="800px" />
+          </div>
+          <div className="mt-4 flex justify-center">
+            <a
+              href={Resumes}
+              download="avantika_resume.pdf"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg"
+            >
+              Download PDF
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
