aee6b60630e10554aded1068014468249e1db3d4
Avantika Khanna <162442171+avanti77@users.noreply.github.com>
2024-10-11 22:15:20 +0530

    Details.js

--- portfolio/src/Details.js
+++ portfolio/src/Details.js
@@ -45,12 +45,7 @@ export const personalDetails = {
   name: "Avantika Khanna",
   tagline: "I am a Technophile.",
   img: profile,
-  about: `I am a final year Btech student at Graphic Era University. I have strong problem solving, analytics,decision making and excellent communication with interpersonal skills. I pay attention to details and am able to interpret data and trends,diagnose problems, and formulate effective actions plans to resolve issues. I am self-driven and motivated with entrepreneurial passion and desire to work in a fast-paced, result driven environment with varied responsibilities.
-
-  • I have scored a combined aggregate score of 8 or above across all semesters
-  • I possess an extremely sound understanding in the basic areas of computer science such as Data structures , Algorithms, Object Oriented Programming and Design.
-  • I have a good command on C/C++.
-  
+  about: `I am a final year Btech student at Graphic Era University. I have strong problem solving, analytics,decision making and excellent communication with interpersonal skills. I pay attention to details and am able to interpret data and trends,diagnose problems, and formulate effective actions plans to resolve issues. I am self-driven and motivated with entrepreneurial passion and desire to work in a fast-paced, result driven environment with varied responsibilities.I have scored a combined aggregate score of 8 or above across all semesters.I possess an extremely sound understanding in the basic areas of computer science such as Data structures , Algorithms, Object Oriented Programming and Design.I have a good command on C/C++.
   Though I don't have much real world experience but I am a keen and quick learner and I am ready to put my 100% in whatever I do.`,
 };
 
